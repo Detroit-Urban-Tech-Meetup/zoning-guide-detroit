69e91209d33b6b226e5f21a1a5803fa537b45177
Jimmy <jmcbroom@posteo.net>
2019-09-10 23:48:13 -0400

    query Airtable nodes for Codes; make pg for each

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,36 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const result = await graphql(`
+    {
+      allAirtable(filter: { table: { eq: "Codes" } }) {
+        edges {
+          node {
+            id
+            data {
+              Name
+              Zone
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  let zones = result.data.allAirtable.edges.map(e => e.node.data)
+
+  zones.forEach(z => {
+    createPage({
+      path: `/zone/${z.Zone}`,
+      component: path.resolve("./src/templates/zone-page.js"),
+      context: {
+        name: z.Name,
+        zone: z.Zone,
+      },
+    })
+  })
+}
